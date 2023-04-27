a93cfb7dac1ac5879e0358fec86909d8476227db
magrelis <caio7547@gmail.com>
2023-04-27 13:52:10 -0300

    final changes

---
+++ src/app/data/Datafake.ts
@@ -0,0 +1,17 @@
+export const DataFake = [
+
+  {
+     "id":1,
+     "photo":"",
+     "title": "saiu novo agente",
+     "description":"ola"
+
+  },
+  {
+    "id":2,
+    "photo":"",
+    "title": "saiu novo agente",
+    "description":"ola"
+
+ }
+]
