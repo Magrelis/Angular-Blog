42c8a260ace83b80f5e61c32426ccd392852962b
magrelis <caio7547@gmail.com>
2023-04-27 13:06:25 -0300

    add dinamics

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
 export class ContentComponent {
-
+  FotoCover: string =
+    'https://th.bing.com/th/id/OIP.hxhoVVkYmosTcqXPcUd-HgHaFX?w=209&h=180&c=7&r=0&o=5&pid=1.7';
+  contentTitle: string = "noticia exemplo";
+  contentDescription: string = 'baifuchmawiucfnsajifvnseihj';
 }
